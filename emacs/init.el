90428c2e8a5f49cbe7b401d75696d6bb1f5b7ab1
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2013-06-20 08:29:36 -0700

    Adding to init.et, cloning emacs.d repo with fresh

--- emacs/init.el
+++ emacs/init.el
@@ -5,6 +5,18 @@
 	     '("marmalade" .  "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar default-packages '(
+			   color-theme-solarized
+			   magit
+			   ))
+
+(dolist (p default-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 (load-theme 'solarized-dark t)
 
 
