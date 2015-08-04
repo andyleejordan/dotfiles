76eff05de34887a8ff5d35282bd5d613e999a4c6
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-08-03 23:05:55 -0700

    Clean up .shell/common.sh

--- .shell/common.sh
+++ .shell/common.sh
@@ -1,22 +1,24 @@
-## path
+# this file is sourced by .bashrc and .zshrc
+
+# path setup
 source ~/.shell/path-edit.sh
 path_front ~/bin /usr/local/bin
 path_back /sbin /bin /usr/sbin /usr/bin
 
-## local settings
-[[ -e ~/.shell/local.sh ]] && source ~/.shell/local.sh
-
-## aliases
+# aliases
 source ~/.shell/aliases.sh
 
 ## disable flow control
 stty -ixon
 
-## liquidprompt
+# liquidprompt
 [[ ( -e ~/.liquidprompt/ ) && ( $- = *i* ) ]] && source ~/.liquidprompt/liquidprompt
 
-## assume colors work
+# assume colors work
 export TERM=xterm-256color
 
-## less options
+# less options
 export LESS='-g -i -M -R -S -w -z-4'
+
+# source everything else
+[[ -e ~/.shell/local.sh ]] && source ~/.shell/local.sh
