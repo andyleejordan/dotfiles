da02b86ced47dc643fb7785e94ca8d2327245e58
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-08-03 23:09:47 -0700

    Clean up path edit

--- .shell/path-edit.sh
+++ .shell/path-edit.sh
@@ -1,5 +1,5 @@
-# Function to take array of paths and add them (cleanly) to the front
-function path_front {
+# function to take array of paths and add them (cleanly) to the front
+path_front() {
     # reverse the args so first path is added last (front-most)
     # uses #36 from http://www.catonmat.net/blog/sed-one-liners-explained-part-one/
     # for emulated tac through sed
@@ -8,18 +8,18 @@ function path_front {
 	PATH=$(echo $PATH | sed -e "s,\(^|:\)${dir}\(:|$\),:,g" \
 	    -e "s,\(^:\)|\(:$\),,g")
 	# adds arg to front of PATH
-        PATH="${dir}:$PATH"
+        PATH="$dir:$PATH"
     done
 }
 
-# Function to take array of paths and them (cleanly) to the back
-function path_back {
+# function to take array of paths and them (cleanly) to the back
+path_back() {
     # no reverse here, last path given is added last (very end)
     for dir; do
 	# removes arg from PATH and removes extraneous colons
 	PATH=$(echo $PATH | sed -e "s,\(^|:\)${dir}\(:|$\),:,g" \
 	    -e "s,\(^:\)|\(:$\),,g")
 	# adds arg to front of PATH
-	PATH="$PATH:${dir}"
+	PATH="$PATH:$dir"
     done
 }
