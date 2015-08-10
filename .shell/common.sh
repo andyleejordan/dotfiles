150bb7d5e31c339ac64e4a10c24ca88aad2a10e4
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2015-08-09 22:39:03 -0700

    Remove liquidprompt, TERM, TMUX_THEME, and LESS
    - Depreacted liquidprompt with custom Bash prompt
    - Tmux sets TERM if needed
    - Tmux theme is deprecated Solarized (need Zenburn)
    - Not sure where the less options came from

--- .shell/common.sh
+++ .shell/common.sh
@@ -8,17 +8,6 @@ path_back /sbin /bin /usr/sbin /usr/bin
 # aliases
 source ~/.shell/aliases.sh
 
-# liquidprompt
-[[ -e ~/.liquidprompt/ && $- = *i* ]] && source ~/.liquidprompt/liquidprompt
-
-# assume colors work
-export TERM=xterm-256color
-
-# set Tmux theme
-export TMUX_THEME=dark
-
-# less options
-export LESS='-g -i -M -R -S -w -z-4'
 # enable ls colors
 if ls --color=auto 2> /dev/null; then
     alias ls="ls --color=auto"
