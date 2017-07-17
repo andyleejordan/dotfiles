5303f3d2fe7b1bb1b572e8512c7d5016e89c7f3e
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2017-07-17 10:31:48 -0700

    Update PowerShell profile

--- powershell/.config/powershell/profile.ps1
+++ powershell/.config/powershell/profile.ps1
@@ -1,16 +1,35 @@
 if (Get-Module PSReadLine) {
-    Set-PSReadLineOption -EditMode Emacs
+  Set-PSReadLineOption -EditMode Emacs
 }
 
-function prompt {
-    $location = ([string]$(get-location)).Replace($HOME, '~')
-   "$location |-/ "
+Import-Module posh-git -ErrorAction SilentlyContinue
+if (Get-Module posh-git) {
+  $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
 }
 
-Set-Alias g git
+Set-Alias alias Set-Alias
+alias g git
+alias grep Select-String
+alias less more
+alias zip Compress-Archive
+alias unzip Expand-Archive
 
-function e {
-    emacsclient --alternate-editor="" $Args
+function which { (Get-Command @args).Source }
+function find { rg -i -uuu -l @args }
+function rg { rg.exe -i --colors 'path:bg:white' @args }
+
+function path_pop([string]$path) {
+  $env:PATH = ($env:PATH.split(';') | ?{ $_ -notmatch [regex]::escape($path) }) -join ';'
+}
+
+function path_front([string]$path) {
+  path_pop($path)
+  $env:PATH = $path,$env:PATH -join ';'
+}
+
+function path_back([string]$path) {
+  path_pop($path)
+  $env:PATH = $env:PATH,$path -join ';'
 }
 
 $local = "$PSScriptRoot/profile_local.ps1"
