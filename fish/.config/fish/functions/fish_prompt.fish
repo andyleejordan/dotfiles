29db9f3b68e4f0fd2e5a19cc4204b27e3e594a16
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2018-02-25 00:17:21 -0800

    More prompt updates
    Put status code first.
    
    Add user (with red-colored root).
    
    Color input prompt cyan.

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -2,13 +2,22 @@ function fish_prompt
   # save the status before overwriting it
   set last_status $status
 
-  # this uses a cached hostname to avoid DNS hiccups
-  set host (set_color blue) '@' (set_color normal) (prompt_hostname)
-
   # only print non-zero exit codes
   if test $last_status -ne 0
-    set status_code ' ' (set_color purple) $last_status (set_color normal)
+    set status_code (set_color magenta) $last_status (set_color normal) ' '
   end
+
+
+  # red root user
+  if test (id -u) -eq 0
+    set user_color red
+  else
+    set user_color normal
+  end
+  set user (set_color $user_color) (whoami) (set_color normal)
+
+  # this uses a cached hostname to avoid DNS hiccups
+  set host (set_color blue) '@' (set_color normal) (prompt_hostname)
 
   # configure git info
   set -g __fish_git_prompt_showdirtystate true
@@ -21,6 +30,8 @@ function fish_prompt
   set -g fish_prompt_pwd_dir_length 5
   set cwd ' ' (set_color blue) (prompt_pwd) (set_color normal)
 
+  set postfix '\n' (set_color cyan) '>' (set_color normal) ' '
+
   # echo without newline or extra spaces, but interpret escapes
-  echo -n -s -e $host $status_code $vcs $cwd '\n> '
+  echo -n -s -e $status_code $user $host $vcs $cwd $postfix
 end
