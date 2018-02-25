061e571b1d851c2e31435915b2c3eb28f7af508b
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2018-02-24 23:45:45 -0800

    Use two-line prompt
    Changed and fixed some colors (was leaking status code color).
    
    Added a newline so paths can be expanded further, and commands always
    start in the same spot.
    
    Removed the superfluous parentheses around the prompt.

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,30 +1,26 @@
 function fish_prompt
   # save the status before overwriting it
-	set last_status $status
-
-  # used to color the prefix and postfix parens
-  set color_parens (set_color green)
-
-  set prefix $color_parens '('
+  set last_status $status
 
   # this uses a cached hostname to avoid DNS hiccups
   set host (set_color blue) '@' (set_color normal) (prompt_hostname)
 
+  # only print non-zero exit codes
   if test $last_status -ne 0
-    set status_code (set_color purple) ' ' $last_status
+    set status_code ' ' (set_color purple) $last_status (set_color normal)
   end
 
+  # configure git info
   set -g __fish_git_prompt_showdirtystate true
   set -g __fish_git_prompt_showupstream auto
   set -g __fish_git_prompt_describe_style branch
   set -g __fish_git_prompt_showcolorhints true
   set vcs (__fish_vcs_prompt) # this has a prefixed space
 
-  # this uses a special shortened path
-  set cwd (set_color normal) ' ' (prompt_pwd)
+  # increase max path component length
+  set -g fish_prompt_pwd_dir_length 5
+  set cwd ' ' (set_color blue) (prompt_pwd) (set_color normal)
 
-  set postfix $color_parens ') '
-
-  # echo without newline or extra spaces
-  echo -n -s $prefix $host $status_code $vcs $cwd $postfix
+  # echo without newline or extra spaces, but interpret escapes
+  echo -n -s -e $host $status_code $vcs $cwd '\n> '
 end
