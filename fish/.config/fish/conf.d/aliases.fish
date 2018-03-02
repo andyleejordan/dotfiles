a75a857282293182e4b715f8e64f83857bb8ff3f
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2018-03-01 23:36:01 -0800

    Alias g to hub when available, and fix completions

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -1,5 +1,15 @@
-alias g 'git'
+if type -q hub
+    alias g hub
+    complete --command hub --wraps git
+    complete --command g --wraps hub
+else
+    alias g git
+    complete --command g --wraps git
+end
+
 alias e 'emacsclient --alternate-editor="" --no-wait'
 alias ec 'e --create-frame'
-alias p 'package'
+
+alias p package
+
 alias rg 'rg --smart-case'
