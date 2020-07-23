ecc67ba68d7cd0eecb49f29e6e4099635aafe868
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2020-07-23 13:05:56 -0700

    Redo emacsclient aliases

--- bash/.config/shell/aliases.sh
+++ bash/.config/shell/aliases.sh
@@ -1,12 +1,14 @@
 # Emacs as editor
-alias e='emacsclient --alternate-editor="" --no-wait'
-# and open a new frame
-alias ec='e --create-frame'
-# in the terminal
-alias et='e --tty'
+alias __emacsclient='emacsclient --alternate-editor="" --no-wait'
+# Create a new frame
+alias e='__emacsclient --create-frame'
+# Or reuse an existing frame
+alias er='__emacsclient'
+# Or open in the terminal
+alias et='__emacsclient --tty'
 
 # create a frame when programs invoke the editor
-export VISUAL=ec EDITOR=ec
+export VISUAL=e EDITOR=e
 
 # p: a common package manager interface
 source ~/.config/shell/packages.sh
