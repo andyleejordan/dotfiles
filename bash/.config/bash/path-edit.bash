675bbd1fce6f7d3f2e56e6f917a5190276aff4fe
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2021-01-26 15:55:29 -0800

    Fix broken path-edit
    Now the pipes for OR need to be escaped.

--- bash/.config/bash/path-edit.bash
+++ bash/.config/bash/path-edit.bash
@@ -5,20 +5,20 @@ path_front() {
   # for emulated tac through sed
   for dir in $(echo $@ | tr " " "\n" | sed '1!G;h;$!d'); do
     # removes arg from PATH and removes extraneous colons
-    PATH=$(echo $PATH | sed -e "s,\(^|:\)${dir}\(:|$\),:,g" \
-                            -e "s,\(^:\)|\(:$\),,g")
+    PATH=$(echo $PATH | sed -e "s,\(^\|:\)${dir}\(:\|$\),:,g" \
+                            -e "s,\(^:\)\|\(:$\),,g")
     # adds arg to front of PATH
     PATH="$dir:$PATH"
   done
 }
 
-# function to take array of paths and them (cleanly) to the back
+# function to take array of paths and add them (cleanly) to the back
 path_back() {
   # no reverse here, last path given is added last (very end)
   for dir; do
     # removes arg from PATH and removes extraneous colons
-    PATH=$(echo $PATH | sed -e "s,\(^|:\)${dir}\(:|$\),:,g" \
-                            -e "s,\(^:\)|\(:$\),,g")
+    PATH=$(echo $PATH | sed -e "s,\(^\|:\)${dir}\(:\|$\),:,g" \
+                            -e "s,\(^:\)\|\(:$\),,g")
     # adds arg to front of PATH
     PATH="$PATH:$dir"
   done
