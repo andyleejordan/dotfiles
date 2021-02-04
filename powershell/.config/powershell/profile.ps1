c104c9367f17be4c18d2aa4e475a5355b1815b6b
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2021-02-04 12:19:27 -0800

    Update PowerShell profile (fixing broken things)

--- powershell/.config/powershell/profile.ps1
+++ powershell/.config/powershell/profile.ps1
@@ -2,80 +2,61 @@ if (Get-Module PSReadLine) {
   Set-PSReadLineOption -EditMode Emacs
 }
 
+function Test-IsAdmin() {
+  if ($IsWindows) {
+    $id = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = [Security.Principal.WindowsPrincipal] $id
+    $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+  } else {
+    $id = id -u
+    $id -eq 0
+  }
+}
+
 Import-Module posh-git -ErrorAction SilentlyContinue
-function prompt {
+
+function prompt() {
   # save the status before overwriting it
   $last_status = $LASTEXITCODE
 
   if (Test-Path variable:/PSDebugContext) {
-    Write-Host "[DBG] " -ForegroundColor Yellow -NoNewLine
+    Write-Host "[DBG] " -ForegroundColor ([ConsoleColor]::Yellow) -NoNewLine
   }
 
   # only print non-zero exit codes
   if ($last_status -ne 0) {
-    Write-Host "$last_status " -ForegroundColor Magenta -NoNewLine
+    Write-Host "$last_status " -ForegroundColor ([ConsoleColor]::Magenta) -NoNewLine
   }
 
-  # red @ for root, otherwise blue
-  $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
-  $principal = [Security.Principal.WindowsPrincipal] $identity
-  if ($principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    $at_color = "Red"
-  } else {
-    $at_color = "Blue"
-  }
-
+  $at_color = if (Test-IsAdmin) { [ConsoleColor]::Red } else { [ConsoleColor]::DarkBlue }
   Write-Host "@" -ForegroundColor $at_color -NoNewLine
-  Write-Host "$(hostname)" -NoNewLine
-
-  # print git info if available
-  if (Get-Module posh-git) {
-    $GitPromptSettings.BeforeText = ' ('
-    $GitPromptSettings.BeforeForegroundColor = [ConsoleColor]::DarkBlue
-    $GitPromptSettings.AfterText = ')'
-    $GitPromptSettings.AfterForegroundColor = [ConsoleColor]::DarkBlue
-    $GitPromptSettings.BranchForegroundColor = [ConsoleColor]::Green
-    $GitPromptSettings.BranchGoneStatusForegroundColor = [ConsoleColor]::Red
-    $GitPromptSettings.BranchIdenticalStatusToForegroundColor = [ConsoleColor]::Green
-    $GitPromptSettings.BranchAheadStatusForegroundColor = [ConsoleColor]::Green
-    $GitPromptSettings.BranchBehindStatusForegroundColor = [ConsoleColor]::Green
-    $GitPromptSettings.BranchBehindAndAheadStatusForegroundColor = [ConsoleColor]::DarkRed
-
-    Write-VcsStatus
-  }
-
-  $regex = [regex]::Escape($HOME) + "(\\.*)*$"
-  $path = $executionContext.SessionState.Path.CurrentLocation.Path -replace $regex, '~$1'
+  Write-Host "$(hostname) " -NoNewLine
+  $regex = "^" + [regex]::Escape($HOME)
+  $path = $executionContext.SessionState.Path.CurrentLocation.Path -replace $regex, '~'
   $path = $path.Replace('\', '/')
-  Write-Host " $path" -ForegroundColor Blue # NewLine
-  Write-Host ">" -ForegroundColor Cyan -NoNewLine
+  Write-Host "$path " -ForegroundColor ([ConsoleColor]::DarkBlue) -NoNewLine
+  if (Get-Command Write-VcsStatus -ErrorAction SilentlyContinue) {
+    $GitPromptSettings.BeforeText = ''
+    $GitPromptSettings.AfterText = ' '
+    $GitPromptSettings.BranchForegroundColor = [ConsoleColor]::DarkGreen
+    $GitPromptSettings.BranchGoneStatusForegroundColor = [ConsoleColor]::DarkRed
+    $GitPromptSettings.BranchIdenticalStatusToForegroundColor = [ConsoleColor]::DarkGreen
+    $GitPromptSettings.BranchAheadStatusForegroundColor = [ConsoleColor]::DarkGreen
+    $GitPromptSettings.BranchBehindStatusForegroundColor = [ConsoleColor]::DarkGreen
+    $GitPromptSettings.BranchBehindAndAheadStatusForegroundColor = [ConsoleColor]::DarkRed
+     Write-VcsStatus
+  }
+  Write-Host "$" -ForegroundColor ([ConsoleColor]::DarkCyan) -NoNewLine
+
   $LASTEXITCODE = $last_status
   return " " # satisfy the `prompt` function
 }
 
-$solarized = "$PSScriptRoot/Set-SolarizedDarkColorDefaults.ps1"
-if (Test-Path $solarized) {
-  . $solarized
-  # Fish-like colors
-  $options.CommandForegroundColor = 'DarkBlue'
-  $options.ContinuationPromptForegroundColor = 'Blue'
-  $options.DefaultTokenForegroundColor = 'Cyan'
-  $options.EmphasisForegroundColor = 'Cyan'
-  $options.StringForegroundColor = 'Yellow'
-}
-
-Set-Alias alias Set-Alias
-
-if (Get-Command -Name hub -ErrorAction SilentlyContinue) {
-    alias g hub
-} else {
-    alias g git
-}
-
-alias grep Select-String
-alias less more
-alias zip Compress-Archive
-alias unzip Expand-Archive
+Set-Alias g git
+Set-Alias grep Select-String
+Set-Alias less more
+Set-Alias zip Compress-Archive
+Set-Alias unzip Expand-Archive
 
 function e { emacsclient -n @args }
 function which { (Get-Command @args).Source }
@@ -89,7 +70,7 @@ function ln {
 
 function Show-Colors() {
   $colors = [Enum]::GetValues([ConsoleColor])
-  $max = ($colors | foreach { "$_ ".Length } | Measure-Object -Maximum).Maximum
+  $max = ($colors | ForEach-Object { "$_ ".Length } | Measure-Object -Maximum).Maximum
   foreach ($color in $colors) {
     Write-Host (" {0,2} {1,$max} " -f [int]$color,$color) -NoNewline
     Write-Host "$color" -Foreground $color
