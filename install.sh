d268ee378dc515d8a39fd2db5ec150e5e9f5e820
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2021-02-19 18:07:06 -0800

    Update bootstrap script

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-sudo apt-get install -y stow
+
+# Install GNU stow
+if ! command -v stow >/dev/null @>&1; then
+    if command -v apt-get >/dev/null 2>&1; then
+        sudo apt-get install -y stow
+    elif command -v brew >/dev/null 2>&1; then
+        brew install stow
+    fi
+fi
+
+# Backup common existing files
 for f in .bashrc .bash_profile .bash_logout; do
     if [ -e ~/$f ]; then
         mv ~/$f ~/$f.bak
     fi
 done
-stow --target="$HOME" --stow bash bin git powershell readline
+
+# Stow dotfiles
+stow --target="$HOME" --stow bash bin emacs git powershell readline
